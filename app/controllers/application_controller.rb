0dcd53fd121cb80b63b83d672beeefddb6ede991
Dalton Dick <daltondick2010@gmail.com>
2013-10-07 19:57:04 -0500

    Set the API key for themoviedb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,25 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  require 'themoviedb'
+
+  before_filter :set_config
+  Tmdb::Api.key(get_config("api.themoviedb"))
+
+  def set_config
+  	@configuration = Tmdb::configuration.new
+  end
+
+  def get_config(value)
+  	file = File.open("./config/apikeys.config", 'r')
+  	contents = file.read.split(/\n/)
+  	config = {}
+  	contents.each do |c|
+  		split = c.split("=")
+  		config[split[0]] = split[1]
+  	end
+
+  	return config[value]
+  end
 end
