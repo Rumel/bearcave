3bef555c2ac015d15ebee29e43ddb2cac341144f
Dalton Dick <daltondick2010@gmail.com>
2013-10-07 19:04:54 -0500

    Allow movie file to be uploaded

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,21 @@
 class AdminController < ApplicationController
 	before_action :signed_in_admin
+	REPLACEMENTS = ["480p", "480i", "720p", "720i", "1080p", "1080i"]
 
 	def show
 	end
+
+	def upload
+		uploaded_file = params[:file]
+		@contents = uploaded_file.read.split("\n")
+	end
+
+	private
+
+		# def treat_uploaded_file(text)
+		# 	REPLACEMENTS.each do |r|
+		# 		text.gsub! r, ""
+		# 	end
+		# 	return text
+		# end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Bearcave::Application.routes.draw do
   match 'signin', to: 'sessions#new', via: 'get'
   match 'signout', to: 'sessions#destroy', via: 'delete'
   match 'admin',  to: 'admin#show', via: 'get'
+  match 'admin/upload', to: 'admin#upload', via: 'post'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
